06645264b263eb529524e525785a6c08a9fac0a6
in-genieur <20160658@gm.hannam.ac.kr>
2021-01-15 01:23:09 +0900

    7강 완료

--- 07/js/quiz-1.js
+++ 07/js/quiz-1.js
@@ -1,6 +1,11 @@
 		var numbers = [2, 4, 6, 8, 10];
 		showArray(numbers);
 
+		var sum = 0
+		numbers.forEach(element => sum += element)
+		numbers.push(sum)
+		showArray(numbers)
+
 		function showArray(arr) {
 			var str = "<table><tr>";
 			for (var i=0; i<arr.length; i++) {
